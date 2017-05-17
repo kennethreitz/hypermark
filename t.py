770dc854eaefb479fdc18fab373658429bf8a175
Kenneth Reitz <me@kennethreitz.org>
2017-05-17 14:26:23 -0400

    changes

--- t.py
+++ t.py
@@ -1,9 +1,7 @@
 import hypermark
 
-from hypermark.bootstrap import session
 content = "# fuck yea\n\n<script></script>\n\nhttp://github.com"
 
 
 d = hypermark.text(content)
-
 print d.filter('transpose_headers', levels=2).html
